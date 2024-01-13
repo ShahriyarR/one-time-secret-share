68fd03f0c3982b93c144c7342fd3daeaa11c2b04
vahidoqtayoglu <vahid.oqtayoglu@gmail.com>
2024-01-13 14:46:03 +0400

    toml file edited

--- src/onetime/entrypoints/web_flask/tests.py
+++ src/onetime/entrypoints/web_flask/tests.py
@@ -53,8 +53,8 @@ class SecretTestCase(FlaskTestCase):
         self.assertIn("/secret", url)
 
     def test_if_secret_url_can_be_created_with_empty_secret(self):
-        response = self.client.post("/", data={"secret": " "})
-        url=self.get_url(response = response)
+        response = self.client.post("/", data={"secret": ""})
+        url=self.get_url(response=response)
         self.assertIs(url, None)
 
     def test_if_secret_url_can_be_opened(self):
